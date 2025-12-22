5ba8df6e48d236b198eb71f9d7b608c3d26d8638
Manjeet <manjeet.s@inte-qt.com>
2025-12-22 19:34:06 +0530

    SMTP timeout

--- backend/routes/formRoutes.js
+++ backend/routes/formRoutes.js
@@ -12,6 +12,7 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  connectionTimeout: 10000,
 });
 
 transporter.verify((error, success) => {
